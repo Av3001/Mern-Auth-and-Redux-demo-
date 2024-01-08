94d7699a03bf5cdfeda0de3c7f1e2e84ee6be7b4
Av3001 <113854539+Av3001@users.noreply.github.com>
2024-01-08 22:23:25 +0530

    checking cicd

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
   );
 } else {
   app.get('/', (req, res) => {
-    res.send('API is running....');
+    res.send('<h1>API is running.... along with cicd</h1>.');
   });
 }
 

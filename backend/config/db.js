4abaa6bcd2f99ef6da7bd4e1a5ce40bf70b0677e
Av3001 <avneetwadhwa87@gmail.com>
2024-01-08 22:00:31 +0530

    fixed database issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/?readPreference=primary&ssl=false&directConnection=true');
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://test:testcluster0.gverq1o.mongodb.net/?retryWrites=true&w=majority');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);

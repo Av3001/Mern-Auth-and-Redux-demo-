45ff94a2d3ac6ed6d68390d213ee771f9255ce0f
Av3001 <avneetwadhwa87@gmail.com>
2024-01-08 21:24:08 +0530

    changed known_hosts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.10.0/bin
 
-cd Mern-Auth-and-Redux-demo-
+ cd Mern-Auth-and-Redux-demo-
  git pull origin main
  cd backend
  pm2 kill

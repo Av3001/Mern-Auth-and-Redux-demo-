097f412a86cc76b83beb047f9ecafdbab16c3bce
Av3001 <avneetwadhwa87@gmail.com>
2024-01-08 22:06:38 +0530

    now fixed database

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://test:testcluster0.gverq1o.mongodb.net/?retryWrites=true&w=majority');
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect('mongodb+srv://test:test@cluster0.gverq1o.mongodb.net/?retryWrites=true&w=majority');
+    console.log(`MongoDB Connected to : ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);

94c5bb996985929a260cac8d19ff6b0a2b98b421
Av3001 <avneetwadhwa87@gmail.com>
2024-01-08 20:06:10 +0530

    fixed bugs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.10.0/bin
 
-cd cd Mern-Auth-and-Redux-demo-
- git pull origin master
+cd Mern-Auth-and-Redux-demo-
+ git pull origin main
  cd backend
  pm2 kill
  pm2 start server.js
